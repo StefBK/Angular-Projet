a7d43deec125d0fba5d7296f574df4047037ac0e
Stef BK <stefsbk@gmail.com>
2022-04-20 12:04:59 +0200

    Blur & Grayscale Buttons added

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -13,6 +13,33 @@ export class DetailsComponent implements OnInit {
   
   itemId="";
   itemInfo:any;
+  imgSrc='';
+  gray=false;
+  blur=0;
+
+  // generateSrc(id=this.itemId,gray=false,blur=0){
+  generateSrc(id=this.itemId,gray=false,blur=0){
+      this.imgSrc='https://picsum.photos/id/'+this.itemId+'/1280/960?';
+    this.gray? this.imgSrc +='grayscale&':null;
+    // Expression ternaire équivaut à :
+    // if(this.gray){
+    //   this.imgSrc+='?grayscale';}
+    // else{
+    //   null;
+    // }
+    this.blur>0? this.imgSrc +='blur='+this.blur:null;
+  }
+
+  grayscale(){
+    this.gray=!this.gray;
+    this.generateSrc();
+    // console.log(this.gray);
+  }
+
+  addBlur(){
+    this.blur<10?this.blur++:this.blur=0;
+    this.generateSrc();
+  }
 
   ngOnInit(): void {
     // On récupère l'id dans la route active
@@ -22,6 +49,7 @@ export class DetailsComponent implements OnInit {
     this.http.get('https://picsum.photos/id/'+this.itemId+'/info').subscribe(
       data=>{
         this.itemInfo=data;
+        this.generateSrc(this.itemId);
         // console.log(data)
         ;}
     );
